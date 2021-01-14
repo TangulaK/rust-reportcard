9baa07f04ec2ba64be5140ca869814672fd086c0
xuri <xuri.me@gmail.com>
2021-01-14 16:37:05 +0800

    Fix broken links for the source file

--- tools/clean_rust_toolchain.sh
+++ tools/clean_rust_toolchain.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd ~/.rustup/toolchains/
-for i in nightly-*; do rustup toolchain remove ${i/%-x86_64-unknown-linux-gnu}; done
+for i in *
+do
+    if [[ ${i} != "stable-x86_64-unknown-linux-gnu" ]]
+    then
+      rustup toolchain remove $i
+    fi
+done
